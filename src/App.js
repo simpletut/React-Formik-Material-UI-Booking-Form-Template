ba9b1ebf68e99b42c27db9fd8a4b1ce743c7b4fa
Ashley Bibizadeh <ashleybibizadeh@Ashleys-MacBook-Pro.local>
2021-01-23 12:37:22 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import Header from './Components/Header';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Container,
+  Grid,
+} from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
 
 const App = () => {
-  return (
-    <div>
+  const classes = useStyles();
 
-    </div>
+  return (
+    <Grid container>
+      <Grid item xs={12}>
+        <Header />
+      </Grid>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+
+
+
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
